690894e295b7e01a3ccd9bb121f80a2884e8e548
Racer <reaganjoseph26@yahoo.com>
2021-07-03 22:33:08 -0400

    nav links js complete

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+// const list = document.querySelectorAll(".list")
+// for (let i = 0; i < list.length; i++) {
+//     list[i].onclick
+// }
+
+const list = document.querySelectorAll(".list")
+for (let i = 0; i < list.length; i++) {
+    list[i].onclick = function () {
+        let j = 0;
+        while(j < list.length) {
+            list[j++].className = 'list'
+        }
+        list[i].className = 'list active';
+    }
+}
+
+//document.querySelector(".list").onlick("click", )
