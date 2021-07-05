edfed700e774848a181374c17f75bdef5a1040a3
Racer <reaganjoseph26@yahoo.com>
2021-07-04 20:41:35 -0400

    navigation complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,10 @@
 const list = document.querySelectorAll(".list")
 const menuToggle = document.querySelector(".toggle")
+const navigation = document.querySelector(".navigation")
 
 menuToggle.onclick = function () {
-    menuToggle.classList.toggle("active")
+    menuToggle.classList.toggle("active");
+    navigation.classList.toggle("active");
 }
 
 for (let i = 0; i < list.length; i++) {
