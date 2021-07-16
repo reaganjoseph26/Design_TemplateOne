394cf16ffd0d128d8137b32b3024f546d6476c76
Racer <reaganjoseph26@yahoo.com>
2021-07-16 02:54:54 -0400

    fontawesome added and dark mode completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,24 @@
-$(document).ready(function() {
-    $('#home-hello').animate({opacity: 1}, 2000, function(){
-         $('#home-slogan').animate({opacity: 1}, 2000, function(){
-              $('#home-slogan2').animate({opacity: 1}, 2000, function(){
+// $(document).ready(function() {
+//     $('#home-hello').animate({opacity: 1}, 2000, function(){
+//          $('#home-slogan').animate({opacity: 1}, 2000, function(){
+//               $('#home-slogan2').animate({opacity: 1}, 2000, function(){
          
-              })
-         })
-    });
+//               })
+//          })
+//     });
 
-    // const themeSwitch = document.querySelector(".themeSwitch");
-    // const body = document.querySelector("body");
+   
+// });
 
-    // //themeSwitch toggle
-    // themeSwitch.onclick = function() {
-    //  body.classList.toggle("dark");
-});
+const themeSwitch = document.querySelector(".theme-switch");
+const homeScreen = document.querySelector(".home-section");
+const homeHeader = document.querySelector(".header");
+
+//themeSwitch toggle
+themeSwitch.onclick = function() {
+ homeScreen.classList.toggle("dark");
+ homeHeader.classList.toggle("dark");
+}
 
 
 // const list = document.querySelectorAll(".list")
