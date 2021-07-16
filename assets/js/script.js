4b0018af584a65c45e5a85970f008df782d55a4e
Racer <reaganjoseph26@yahoo.com>
2021-07-16 01:27:20 -0400

    aos style added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,36 @@
-const list = document.querySelectorAll(".list")
-const menuToggle = document.querySelector(".toggle")
-const navigation = document.querySelector(".navigation")
+$(document).ready(function() {
+    $('#home-hello').animate({opacity: 1}, 2000, function(){
+         $('#home-slogan').animate({opacity: 1}, 2000, function(){
+              $('#home-slogan2').animate({opacity: 1}, 2000, function(){
+         
+              })
+         })
+    });
 
-menuToggle.onclick = function () {
-    menuToggle.classList.toggle("active");
-    navigation.classList.toggle("active");
-}
+    // const themeSwitch = document.querySelector(".themeSwitch");
+    // const body = document.querySelector("body");
 
-for (let i = 0; i < list.length; i++) {
-    list[i].onclick = function () {
-        let j = 0;
-        while(j < list.length) {
-            list[j++].className = 'list'
-        }
-        list[i].className = 'list active';
-    }
-}
+    // //themeSwitch toggle
+    // themeSwitch.onclick = function() {
+    //  body.classList.toggle("dark");
+});
+
+
+// const list = document.querySelectorAll(".list")
+// const menuToggle = document.querySelector(".toggle")
+// const navigation = document.querySelector(".navigation")
+
+// menuToggle.onclick = function () {
+//     menuToggle.classList.toggle("active");
+//     navigation.classList.toggle("active");
+// }
+
+// for (let i = 0; i < list.length; i++) {
+//     list[i].onclick = function () {
+//         let j = 0;
+//         while(j < list.length) {
+//             list[j++].className = 'list'
+//         }
+//         list[i].className = 'list active';
+//     }
+// }
